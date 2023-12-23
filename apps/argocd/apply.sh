3be570fa451040ac4c8630729b0feb09e0de19ce
Gavin Mogan <git@gavinmogan.com>
2023-12-23 11:19:33 -0800

    disable argocd https mode, so ingress can proxy easily

--- apps/argocd/apply.sh
+++ apps/argocd/apply.sh
@@ -1,1 +1,1 @@
-helm upgrade -i argo-cd . --namespace argocd --values values.yaml --values secrets://values-secrets.yaml
+helm upgrade -i argo-cd . --namespace argocd --create-namespace --values values.yaml --values secrets://values-secrets.yaml
