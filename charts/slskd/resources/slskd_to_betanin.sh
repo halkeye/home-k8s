758c0002c92973771c976cca3da5358bde38a2c0
Gavin Mogan <git@gavinmogan.com>
2025-05-08 21:44:17 -0700

    cleanup

--- charts/slskd/resources/slskd_to_betanin.sh
+++ charts/slskd/resources/slskd_to_betanin.sh
@@ -3,7 +3,8 @@
 name=$(echo "$1" | sed 's/.*"localDirectoryName":"\/downloads\/slskd\/\([^"]*\)".*/\1/')
 
 wget -q -O/dev/null \
-     --post-data "name=$name&path=/downloads" \
+     --data-urlencode "path=/downloads" \
+     --data-urlencode "name=$name" \
      --header="X-API-KEY: ${BETANIN_API_KEY}" \
      --header="User-Agent: slskd_to_betanin.sh" \
      "${BETANIN_URL}/api/torrents"
