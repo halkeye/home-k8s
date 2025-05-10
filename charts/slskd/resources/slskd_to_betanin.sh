d116081f2298aa70ab3b13f3a2f76cb4576b40f2
Gavin Mogan <git@gavinmogan.com>
2025-05-10 00:47:35 -0700

    use jq to get name better

--- charts/slskd/resources/slskd_to_betanin.sh
+++ charts/slskd/resources/slskd_to_betanin.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-name=$(echo "$1" | sed 's/.*"localDirectoryName":"\/downloads\/slskd\/\([^"]*\)".*/\1/')
+#name=$(echo "$1" | sed 's/.*"localDirectoryName":"\/downloads\/slskd\/\([^"]*\)".*/\1/')
+name=$(echo "$1" | jq .localDirectoryName -r)
 
 wget -q -O/dev/null \
-     --post-data "name=$name&path=/downloads" \
-     --header="X-API-KEY: ${BETANIN_API_KEY}" \
-     --header="User-Agent: slskd_to_betanin.sh" \
-     "${BETANIN_URL}/api/torrents"
+  --post-data "name=$name&path=/downloads" \
+  --header="X-API-KEY: ${BETANIN_API_KEY}" \
+  --header="User-Agent: slskd_to_betanin.sh" \
+  "${BETANIN_URL}/api/torrents"
