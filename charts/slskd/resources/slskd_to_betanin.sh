ec9c2360322792f9487c9c4364a55679c3ea0794
Gavin Mogan <git@gavinmogan.com>
2025-05-10 00:55:14 -0700

    fix paths and api key

--- charts/slskd/resources/slskd_to_betanin.sh
+++ charts/slskd/resources/slskd_to_betanin.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #name=$(echo "$1" | sed 's/.*"localDirectoryName":"\/downloads\/slskd\/\([^"]*\)".*/\1/')
-name=$(echo "$1" | jq .localDirectoryName -r)
+name=$(echo "$1" | jq .localDirectoryName -r | sed 's/\/downloads\/slskd\///')
 
 wget -q -O/dev/null \
-  --post-data "name=$name&path=/downloads" \
+  --post-data "name=$name&path=/downloads/slskd" \
   --header="X-API-KEY: ${BETANIN_API_KEY}" \
   --header="User-Agent: slskd_to_betanin.sh" \
   "${BETANIN_URL}/api/torrents"
