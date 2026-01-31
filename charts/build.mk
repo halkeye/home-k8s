038f84ad3dec0d7d33c15daecfc484136127a0f7
Gavin Mogan <git@gavinmogan.com>
2026-01-31 09:24:22 -0800

    makefiles

---
+++ charts/build.mk
@@ -0,0 +1,17 @@
+# Define a variable for the shell to use 'printf' correctly
+SHELL := /bin/bash
+
+# Define the help target as PHONY, meaning it's not a file
+.PHONY: help diff all
+
+help: ## Display this help message
+	@printf "Usage:\n  make <target>\n\nTargets:\n"
+	@grep -E '^[a-zA-Z0-9 -]+:.*##'  Makefile | sort | while read -r l; do printf "\033[1;32m$$(echo $$l | cut -f 1 -d':')\033[00m:$$(echo $$l | cut -f 3- -d'#')\n"; done
+
+all: diff ## Default target to diff everything
+
+diff: ## Show difference between local and dev
+	kustomize build --enable-helm . | kubectl diff -f -
+
+apply: ## Apply
+	kustomize build --enable-helm . | kubectl apply -f -
