1ade2d3325f9bc1ae8cc632ede80eb0d1bdb3f61
Gavin Mogan <git@gavinmogan.com>
2026-02-02 23:21:13 -0800

    dashboardssssssss

--- charts/build.mk
+++ charts/build.mk
@@ -1,5 +1,6 @@
 # Define a variable for the shell to use 'printf' correctly
 SHELL := /bin/bash
+CLUSTER ?= home-k8s
 
 # Define the help target as PHONY, meaning it's not a file
 .PHONY: help diff all
@@ -11,10 +12,10 @@ help: ## Display this help message
 all: diff ## Default target to diff everything
 
 diff: ## Show difference between local and dev
-	kustomize build --helm-api-versions monitoring.coreos.com/v1 --enable-helm . | kubectl diff -f -
+	kustomize build --helm-api-versions monitoring.coreos.com/v1 --enable-helm . | kubectl diff --context $(CLUSTER) -f -
 
 apply: ## Apply
-	kustomize build --helm-api-versions monitoring.coreos.com/v1 --enable-helm . | kubectl apply -f -
+	kustomize build --helm-api-versions monitoring.coreos.com/v1 --enable-helm . | kubectl apply --context $(CLUSTER) -f -
 
 build: ## Build
-	kustomize build --helm-api-versions monitoring.coreos.com/v1 --enable-helm .
+	kustomize build --helm-api-versions monitoring.coreos.com/v1 --enable-helm . --context $(CLUSTER)
